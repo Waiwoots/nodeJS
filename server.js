a5ea2b25ee34bbefe835711d674779503948c9ac
Waiwoot <125531460+Waiwoots@users.noreply.github.com>
2024-10-30 20:18:14 +0700

    Update server.js
    to test server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
   Created by Eric N. (ThatProject)
 */
 /////////////////////////////////////////////////////////////////
-
+/*
 const path = require("path");
 const express = require("express");
 const WebSocket = require("ws");
@@ -46,3 +46,23 @@ app.get("/audio", (req, res) =>
 app.listen(HTTP_PORT, () =>
   console.log(`HTTP server listening at http://localhost:${HTTP_PORT}`)
 );
+*/
+const express = require('express');
+const socketIO = require('socket.io');
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const io = require("socket.io")(server,{
+  cors: {
+    origins: "*:*",
+    methods: ["GET", "POST"]
+  }
+});
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on('messaged', (args) => {
+    io.emit('message', args);
+    console.log(args)
+  });
