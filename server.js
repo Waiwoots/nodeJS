d732b9041b2275a1201ab0b0645cc0a3e46d3d9f
Waiwoot <125531460+Waiwoots@users.noreply.github.com>
2024-10-30 22:04:44 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
+/////////////////////////////////////////////////////////////////
+/*
+  Broadcasting Your Voice with ESP32-S3 & INMP441
+  For More Information: https://youtu.be/qq2FRv0lCPw
+  Created by Eric N. (ThatProject)
+*/
+/////////////////////////////////////////////////////////////////
+
 const path = require("path");
 const express = require("express");
 const WebSocket = require("ws");
 const app = express();
 
 const WS_PORT = process.env.WS_PORT || 8888;
-// const HTTP_PORT = process.env.HTTP_PORT || 8000;
+const HTTP_PORT = process.env.HTTP_PORT || 8000;
 
 const wsServer = new WebSocket.Server({ port: WS_PORT }, () =>
   console.log(`WS server is listening at ws://localhost:${WS_PORT}`)
